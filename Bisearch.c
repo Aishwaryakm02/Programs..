8da9df7971586995c5494d62b801b948e1bd50c0
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-05-04 23:42:25 +0530

    Add binary search implementation in Bisearch.c
    Implement binary search to find an element in a sorted array.

--- Bisearch.c
+++ Bisearch.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int a[] = {1, 4, 5, 6, 7}; // sorted
+    int ele = 7;
+    int low = 0, high = 4;
+    int mid;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (a[mid] < ele) {
+            low = mid + 1;
+        }
+        else if (a[mid] > ele) {
+            high = mid - 1;
+        }
+        else {
+            printf("Element found at index %d", mid);
+            return 0;
+        }
+    }
+
+    printf("Element not found");
+    return 0;
+}
