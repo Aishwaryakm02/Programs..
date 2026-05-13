3f0cd322064557885d9439fdfd7563f9eabcaa50
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-05-13 23:13:49 +0530

    Initialize 2D array and fix loop syntax
    Fix the initialization of the 2D array and correct the loop increment syntax.

--- 2Darr.c
+++ 2Darr.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+
+int main() {
+    int marks[3][2]={1,2,3,4,5,6};
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++){
+            printf("%d",marks[i][j]);
+        }
+        printf("\n\");
+    }
+
+    return 0;
+    }
